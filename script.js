6aa6c59a47dc214cd262be59bddf1545ea420cb4
Sabr Suleiman <sbrsuleiman@gmail.com>
2021-07-14 22:33:32 -0400

    Added 'Clear All' button

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let addToDoButton = document.querySelector('.add_todo');
 let toDoContainer = document.querySelector('.todos');
 let inputField = document.querySelector('.input_todo');
+let clearButton = document.querySelector('.clear_all');
 let countList = 1;
 
 
 addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    addItemToList(paragraph, inputField, toDoContainer);
-    countList++;
-    clearInput(inputField);
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = 'line-through';
-    })
-    paragraph.addEventListener('dblclick', function(){
-        clearItem(toDoContainer, paragraph);
-    })
+    if (inputField.value != '') {
+        var paragraph = document.createElement('p');
+        addItemToList(paragraph, inputField, toDoContainer);
+        countList++;
+        clearInput(inputField);
+        paragraph.addEventListener('click', function(){
+            paragraph.style.textDecoration = 'line-through';
+        })
+        paragraph.addEventListener('dblclick', function(){
+            clearItem(toDoContainer, paragraph);
+        })
+    }
 })
+
+clearButton.addEventListener('click', function() {
+    clearAllItems(toDoContainer);
+});
 
 function addItemToList(paragraph, inputField, toDoContainer) {
     paragraph.classList.add('todo_styling');
@@ -30,4 +37,9 @@ function clearInput(inputField) {
 function clearItem(toDoContainer, paragraph) {
     toDoContainer.removeChild(paragraph);
     countList--;
+}
+
+function clearAllItems(toDoContainer) {
+    toDoContainer.innerHTML = '';
+    countList = 1;
 }
